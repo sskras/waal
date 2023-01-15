a91f5cf9a924acffb686d7c493f2abe88bd09284
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-16 00:44:01 +0200

    Klaipeda-oro-salygos.sh: Convert wind speed inside AWK too
    Eliminate another temporary var + now unused rounding fn.

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-round ()
-{
-    awk '{ printf("%.0f", $1) }'
-}
-
 FULL_HTML=""
 PART_OF_HTML=""
 WIND_DIR_LYKLP=""
@@ -19,9 +14,8 @@ WSKLP_KT=""
 PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -Ezo 'port_weather_header_title.*port_weather_wind_speed_chart_title'| sed '1,14d;25,60d')
 #| sed -r 's/\s+//g'
 WIND_DIR_LYKLP=$(echo "$PART_OF_HTML" | awk '{ printf("%.0f", $1); exit }')
-WIND_SPEED_LYKLP=$(echo "$PART_OF_HTML" | awk 'END { printf("%.2f", $1) }')
+WIND_SPEED_LYKLP=$(echo "$PART_OF_HTML" | awk 'END { WSKLP_KT = $1 * '$MS_CONVERSION_KT'; printf("%.0f", WSKLP_KT) }')
 #echo "$WSKLP_NUM"
-WSKLP_KT=$(echo "scale=2; $WIND_SPEED_LYKLP * $MS_CONVERSION_KT" | bc | round)
 #echo "$WIND_DIR_LYKLP/$WIND_SPEED_LYKLP=${WSKLP_KT}kt"
-echo "$WIND_DIR_LYKLP/${WSKLP_KT}kt"
+echo "$WIND_DIR_LYKLP/${WIND_SPEED_LYKLP}kt"
 #echo "$PART_OF_HTML"
