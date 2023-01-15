2002ea00833352efb93abedc527a904c0cb064fe
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-16 00:38:32 +0200

    Klaipeda-oro-salygos.sh: No need for m/s unit symbols too.
    Eliminate a temporary var.

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -19,10 +19,9 @@ WSKLP_KT=""
 PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -Ezo 'port_weather_header_title.*port_weather_wind_speed_chart_title'| sed '1,14d;25,60d')
 #| sed -r 's/\s+//g'
 WIND_DIR_LYKLP=$(echo "$PART_OF_HTML" | awk '{ printf("%.0f", $1); exit }')
-WIND_SPEED_LYKLP=$(echo "$PART_OF_HTML" | awk 'END { printf("%.2f%s", $1, $2) }')
-WSKLP_NUM=$(echo "${WIND_SPEED_LYKLP%???}")
+WIND_SPEED_LYKLP=$(echo "$PART_OF_HTML" | awk 'END { printf("%.2f", $1) }')
 #echo "$WSKLP_NUM"
-WSKLP_KT=$(echo "scale=2; $WSKLP_NUM * $MS_CONVERSION_KT" | bc | round)
+WSKLP_KT=$(echo "scale=2; $WIND_SPEED_LYKLP * $MS_CONVERSION_KT" | bc | round)
 #echo "$WIND_DIR_LYKLP/$WIND_SPEED_LYKLP=${WSKLP_KT}kt"
 echo "$WIND_DIR_LYKLP/${WSKLP_KT}kt"
 #echo "$PART_OF_HTML"
