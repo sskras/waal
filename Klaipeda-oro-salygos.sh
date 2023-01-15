6a52cddef8720d8f95f23d942452332a519b86bb
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-16 01:43:31 +0200

    Klaipeda-oro-salygos.sh: Reindent the AWK statements.
    Also print the results and exit as early as possible (save the CPU time).

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -2,10 +2,27 @@
 
 MS_CONVERSION_KT=1.94
 
-curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | awk '
-    /port_weather_header_title/ { BLOCK_START = NR }
-    !BLOCK_START { next }
-    NR == BLOCK_START + 14 { WIND_DIR_LYKLP = $1 }
-    NR == BLOCK_START + 23 { WIND_SPEED_LYKLP = $1 * '$MS_CONVERSION_KT' }
-    END { printf("%.0f/%.0fkt\n", WIND_DIR_LYKLP, WIND_SPEED_LYKLP) }
+curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | awk \
+'
+    /port_weather_header_title/
+    {
+        BLOCK_START = NR
+    }
+
+    !BLOCK_START
+    {
+        next
+    }
+
+    NR == BLOCK_START + 14
+    {
+        WIND_DIR_LYKLP = $1
+    }
+
+    NR == BLOCK_START + 23
+    {
+        WIND_SPEED_LYKLP = $1 * '$MS_CONVERSION_KT'
+        printf("%.0f/%.0fkt\n", WIND_DIR_LYKLP, WIND_SPEED_LYKLP)
+        exit
+    }
 '
