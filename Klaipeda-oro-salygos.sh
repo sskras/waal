5ac71f06a70e17575913187bd0be37f999b4ea33
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-16 01:32:28 +0200

    Klaipeda-oro-salygos.sh: Remove unused Bash variables

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-FULL_HTML=""
-PART_OF_HTML=""
-WIND_DIR_LYKLP=""
-WIND_SPEED_LYKLP=""
 MS_CONVERSION_KT=1.94
-WSKLP_NUM=""
-WSKLP_KT=""
-#FULL_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/)
-#FULL_HTML=$(wget -q --output-document - https://portofklaipeda.lt/uostas/oro-salygos/ | tr -d '\000')
-#TMP=$(echo "$FULL_HTML" )
 
-PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -Ezo 'port_weather_header_title.*port_weather_wind_speed_chart_title'| sed '1,14d;25,60d')
 curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | awk '
     /port_weather_header_title/ { BLOCK_START = NR }
     !BLOCK_START { next }
