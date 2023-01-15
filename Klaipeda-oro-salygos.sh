1c4b4f109ce05779297acc2aea19d50c12288541
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-15 22:34:51 +0200

    Klaipeda-oro-salygos.sh: Try rounding results to an integer

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -13,11 +13,11 @@ WSKLP_KT=""
 
 PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -Ezo 'port_weather_header_title.*port_weather_wind_speed_chart_title'| sed '1,14d;25,60d')
 #| sed -r 's/\s+//g'
-WIND_DIR_LYKLP=$(echo "$PART_OF_HTML" | head -n 1 - | sed -r 's/\s+//g')
+WIND_DIR_LYKLP=$(echo "$PART_OF_HTML" | head -n 1 - | sed -r 's/\s+//g' | awk '{ printf("%.0f", $1) }')
 WIND_SPEED_LYKLP=$(echo "$PART_OF_HTML" | tail -n 1 - | sed -r 's/\s+//g')
 WSKLP_NUM=$(echo "${WIND_SPEED_LYKLP%???}")
 #echo "$WSKLP_NUM"
-WSKLP_KT=$(echo "scale=2; $WSKLP_NUM * $MS_CONVERSION_KT" | bc)
+WSKLP_KT=$(echo "scale=2; $WSKLP_NUM * $MS_CONVERSION_KT" | bc | awk '{ printf("%.0f", $1) }')
 #echo "$WIND_DIR_LYKLP/$WIND_SPEED_LYKLP=${WSKLP_KT}kt"
 echo "$WIND_DIR_LYKLP/${WSKLP_KT}kt"
 #echo "$PART_OF_HTML"
