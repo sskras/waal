c425b7af4df83658ab807d767b018c0dcaec2bba
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-16 00:57:30 +0200

    Klaipeda-oro-salygos.sh: Extract and print values in a single AWK run

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -13,9 +13,11 @@ WSKLP_KT=""
 
 PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -Ezo 'port_weather_header_title.*port_weather_wind_speed_chart_title'| sed '1,14d;25,60d')
 #| sed -r 's/\s+//g'
-WIND_DIR_LYKLP=$(echo "$PART_OF_HTML" | awk 'NR == 1 { printf("%.0f", $1) }')
-WIND_SPEED_LYKLP=$(echo "$PART_OF_HTML" | awk 'END { WSKLP_KT = $1 * '$MS_CONVERSION_KT'; printf("%.0f", WSKLP_KT) }')
 #echo "$WSKLP_NUM"
 #echo "$WIND_DIR_LYKLP/$WIND_SPEED_LYKLP=${WSKLP_KT}kt"
-echo "$WIND_DIR_LYKLP/${WIND_SPEED_LYKLP}kt"
+echo "$PART_OF_HTML" | awk '
+    NR == 1 { WIND_DIR_LYKLP = $1 }
+    END { WIND_SPEED_LYKLP = $1 * '$MS_CONVERSION_KT' }
+    END { printf("%.0f/%.0fkt\n", WIND_DIR_LYKLP, WIND_SPEED_LYKLP) }
+'
 #echo "$PART_OF_HTML"
