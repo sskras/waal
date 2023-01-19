8db73c08669214f038c2b3072dded5ae0df0907f
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-19 21:35:42 +0200

    Klaipeda-oro-salygos.sh: Eliminate `sed` by parsing the input in Bash

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -2,12 +2,14 @@
 
 MS_CONVERSION_KT=1.94
 
-PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -zo 'port_weather_header_title.*port_weather_wind_speed_chart_title'| sed -n '15p; 24p')
+PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -zo 'port_weather_header_title.*port_weather_wind_speed_chart_title')
 
-{
-    read WIND_DIR_LYKLP UNITS
-    read WIND_SPEED_LYKLP UNITS
-} <<< $PART_OF_HTML
+while read LINE
+do
+    (( LINE_NUM++ ))
+    [ $LINE_NUM = 15 ] && read WIND_DIR_LYKLP   UNITS <<< "$LINE"
+    [ $LINE_NUM = 24 ] && read WIND_SPEED_LYKLP UNITS <<< "$LINE"
+done <<< $PART_OF_HTML
 
 WSKLP_KT=$(bc <<< "scale=2; $WIND_SPEED_LYKLP * $MS_CONVERSION_KT")
 
