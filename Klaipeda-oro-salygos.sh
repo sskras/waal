1e9b2bc69b01170eaad5a0e2c02b8ad0ae56bd3b
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-19 22:01:58 +0200

    Klaipeda-oro-salygos.sh: Use Bash RE matching instead of grep

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -2,14 +2,17 @@
 
 MS_CONVERSION_KT=1.94
 
+FULL_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/)
 PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -zo 'port_weather_header_title.*port_weather_wind_speed_chart_title')
+
+[[ $FULL_HTML =~ (port_weather_header_title.*port_weather_wind_speed_chart_title) ]] || exit
 
 while read LINE
 do
     (( LINE_NUM++ ))
     [ $LINE_NUM = 15 ] && read WIND_DIR_LYKLP   UNITS <<< "$LINE"
     [ $LINE_NUM = 24 ] && read WIND_SPEED_LYKLP UNITS <<< "$LINE"
-done <<< $PART_OF_HTML
+done <<< ${BASH_REMATCH[1]}
 
 WSKLP_KT=$(bc <<< "scale=2; $WIND_SPEED_LYKLP * $MS_CONVERSION_KT")
 
