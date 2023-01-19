005c0b5e761649ce539a3966c2f65398acc5e42a
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-20 00:22:31 +0200

    Klaipeda-oro-salygos.sh: Fix a thinko / regression.
    No \n are allowed before /.../ and {...}

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -4,23 +4,19 @@ MS_CONVERSION_KT=1.94
 
 curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | awk \
 '
-    /port_weather_header_title/
-    {
+    /port_weather_header_title/ {
         BLOCK_START = NR
     }
 
-    !BLOCK_START
-    {
+    !BLOCK_START {
         next
     }
 
-    NR == BLOCK_START + 14
-    {
+    NR == BLOCK_START + 14 {
         WIND_DIR_LYKLP = $1
     }
 
-    NR == BLOCK_START + 23
-    {
+    NR == BLOCK_START + 23 {
         WIND_SPEED_LYKLP = $1 * '$MS_CONVERSION_KT'
         printf("%.0f/%.0fkt\n", WIND_DIR_LYKLP, WIND_SPEED_LYKLP)
         exit
