e822818c2d76333742cc2e6c7840e5d7db948708
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-19 21:24:04 +0200

    Klaipeda-oro-salygos.sh: Get rid of var initialization

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-FULL_HTML=""
-PART_OF_HTML=""
-WIND_DIR_LYKLP=""
-WIND_SPEED_LYKLP=""
 MS_CONVERSION_KT=1.94
-WSKLP_NUM=""
-WSKLP_KT=""
 
 PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -zo 'port_weather_header_title.*port_weather_wind_speed_chart_title'| sed -n '15p; 24p')
 
@@ -16,4 +10,5 @@ PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -zo 
 } <<< $PART_OF_HTML
 
 WSKLP_KT=$(bc <<< "scale=2; $WIND_SPEED_LYKLP * $MS_CONVERSION_KT")
+
 echo "$WIND_DIR_LYKLP/${WSKLP_KT}kt"
