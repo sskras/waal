1b1699c242464c04263d66e838448aa43578856e
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-19 21:23:09 +0200

    Klaipeda-oro-salygos.sh: Get rid of old comments

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -7,12 +7,8 @@ WIND_SPEED_LYKLP=""
 MS_CONVERSION_KT=1.94
 WSKLP_NUM=""
 WSKLP_KT=""
-#FULL_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/)
-#FULL_HTML=$(wget -q --output-document - https://portofklaipeda.lt/uostas/oro-salygos/ | tr -d '\000')
-#TMP=$(echo "$FULL_HTML" )
 
 PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -zo 'port_weather_header_title.*port_weather_wind_speed_chart_title'| sed -n '15p; 24p')
-#| sed -r 's/\s+//g'
 
 {
     read WIND_DIR_LYKLP UNITS
@@ -20,6 +16,4 @@ PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -zo 
 } <<< $PART_OF_HTML
 
 WSKLP_KT=$(bc <<< "scale=2; $WIND_SPEED_LYKLP * $MS_CONVERSION_KT")
-#echo "$WIND_DIR_LYKLP/$WIND_SPEED_LYKLP=${WSKLP_KT}kt"
 echo "$WIND_DIR_LYKLP/${WSKLP_KT}kt"
-#echo "$PART_OF_HTML"
