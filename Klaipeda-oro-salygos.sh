9c33aa349e3827cca6428c444de3527437228c21
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-19 22:04:37 +0200

    Klaipeda-oro-salygos.sh: Get rid of `grep` matching

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -3,7 +3,6 @@
 MS_CONVERSION_KT=1.94
 
 FULL_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/)
-PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -zo 'port_weather_header_title.*port_weather_wind_speed_chart_title')
 
 [[ $FULL_HTML =~ (port_weather_header_title.*port_weather_wind_speed_chart_title) ]] || exit
 
