39fae2c2dda60cf6a059c0fea9888e5f8e9e0e2b
Saulius Krasuckas <saulius2@ar-fi.lt>
2023-01-19 20:18:41 +0200

    Klaipeda-oro-salygos.sh: Print only useful lines in sed
    ... by using their numbers

--- Klaipeda-oro-salygos.sh
+++ Klaipeda-oro-salygos.sh
@@ -11,7 +11,7 @@ WSKLP_KT=""
 #FULL_HTML=$(wget -q --output-document - https://portofklaipeda.lt/uostas/oro-salygos/ | tr -d '\000')
 #TMP=$(echo "$FULL_HTML" )
 
-PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -Ezo 'port_weather_header_title.*port_weather_wind_speed_chart_title'| sed '1,14d;25,60d')
+PART_OF_HTML=$(curl -s https://portofklaipeda.lt/uostas/oro-salygos/ | grep -Ezo 'port_weather_header_title.*port_weather_wind_speed_chart_title'| sed -n '15p; 24p')
 #| sed -r 's/\s+//g'
 WIND_DIR_LYKLP=$(echo "$PART_OF_HTML" | head -n 1 - | sed -r 's/\s+//g')
 WIND_SPEED_LYKLP=$(echo "$PART_OF_HTML" | tail -n 1 - | sed -r 's/\s+//g')
